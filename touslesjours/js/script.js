4837314d09fcf87626f0233547aeb05659da725b
sohi0000 <sohi0000@naver.com>
2020-10-27 14:08:38 +0900

    2020-10-27 작업

---
+++ touslesjours/js/script.js
@@ -0,0 +1,64 @@
+$(document).ready(function(){
+    
+    $(".slider-wrap1").slick({
+        
+        slidesToShow: 3,
+        infinite: true,
+        arrows: true
+        
+    });
+    
+    $(".slider-wrap2").slick({
+        
+        infinite: true,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
+        arrows: true
+        
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
