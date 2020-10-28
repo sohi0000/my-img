843b51e845baad40b09b185541af93c0b247bd7d
sohi0000 <sohi0000@naver.com>
2020-10-28 17:54:13 +0900

    2020-10-28 작업

--- touslesjours/js/script.js
+++ touslesjours/js/script.js
@@ -1,77 +1,55 @@
-$(document).ready(function(){
-    
-    $(".slider-wrap1").slick({
-        
-        slidesToShow: 3,
-        infinite: true,
-        arrows: true
-        
-    });
-    
-    $(".slider-wrap2").slick({
-        
-        infinite: true,
-        dots: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        pauseOnHover: true,
-        arrows: true
-        
-    });
-    
-    
-    $(window).scroll(function () {
-        
-        
-        console.log($(this).scrollTop());
+$(document).ready(function () {
+
+$(".slider-wrap1").slick({
+
+    slidesToShow: 3,
+    infinite: true,
+    arrows: true
+
+});
+
+$(".slider-wrap2").slick({
+
+    infinite: true,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true,
+    arrows: true
+
+});
+
+
+$(window).scroll(function () {
+
+
+    console.log($(this).scrollTop());
 
     if ($(this).scrollTop() > 900) {
-      $("#header").css({ display : "none" });
-      $("#header-color").css({ display : "block" });
+        $("#header").css({
+            display: "none"
+        });
+        $("#header-color").css({
+            display: "block"
+        });
     } else {
-      $("#header").css({ display : "block" });
-      $("#header-color").css({ display : "none" });
+        $("#header").css({
+            display: "block"
+        });
+        $("#header-color").css({
+            display: "none"
+        });
     }
-        
-        
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+});
+
+
+
+
+
+
+
+
+
 });
