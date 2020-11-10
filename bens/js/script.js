7c800e5ebd05c4b1d7824503cdf9be61c70f2e09
sohi0000 <sohi0000@naver.com>
2020-11-11 00:04:47 +0900

    2020-11-10 작업

--- bens/js/script.js
+++ bens/js/script.js
@@ -1,26 +1,29 @@
-$(document).ready(function(){
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-});
+$(document).ready(function () {
+
+
+            $(".hd-tp-right ul li:last-child i").click(function () {
+                $(".hd-tp-right ul li:last-child").toggleClass("active");
+            });
+
+
+            $('.slider').slick({
+
+                    dots: true,
+                    autoplay: true,
+                    autoplaySpeed: 3000,
+                    pauseOnHover: true,
+                    fade: true,
+                    infinite: true,
+                    arrows: true,
+                    customPaging: function (slick, index) {
+                        return '<div class="page-btn"></div>';}
+
+                    });
+
+
+
+
+
+
+
+            });
