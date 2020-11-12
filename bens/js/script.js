99563b74d47469ac174957a3a36917fa7162b9aa
sohi0000 <sohi0000@naver.com>
2020-11-13 03:47:03 +0900

    2020-11-12 작업

--- bens/js/script.js
+++ bens/js/script.js
@@ -1,153 +1,151 @@
 /* 기능 */
 function SliderK__show($slider, index) {
-  var $currentSlide = $slider.find('.slides > div.active');
-  var $postSlide = $slider.find('.slides > div').eq(index);
+    var $currentSlide = $slider.find('.slides > div.active');
+    var $postSlide = $slider.find('.slides > div').eq(index);
 
-  $currentSlide.removeClass('active');
-  $postSlide.addClass('active');
+    $currentSlide.removeClass('active');
+    $postSlide.addClass('active');
 
-  $slider.find('.page-nav > div > div > div.active').removeClass('active');
-  $slider.find('.page-nav > div > div > div').eq(index).addClass('active');
+    $slider.find('.page-nav > div > div > div.active').removeClass('active');
+    $slider.find('.page-nav > div > div > div').eq(index).addClass('active');
 
-  var $pageBtnUnderline = $slider.find('.page-nav > div > section');
+    var $pageBtnUnderline = $slider.find('.page-nav > div > section');
 
-  var $postPageBtn = $slider.find('.page-nav > div > div > div.active');
-  var pageBtnUnderlineWidth = $postPageBtn.outerWidth();
-  var pageBtnUnderlinePosition = $postPageBtn.position();
+    var $postPageBtn = $slider.find('.page-nav > div > div > div.active');
+    var pageBtnUnderlineWidth = $postPageBtn.outerWidth();
+    var pageBtnUnderlinePosition = $postPageBtn.position();
 
-  $pageBtnUnderline.css({
-    width:pageBtnUnderlineWidth,
-    left:pageBtnUnderlinePosition.left
-  });
+    $pageBtnUnderline.css({
+        width: pageBtnUnderlineWidth,
+        left: pageBtnUnderlinePosition.left
+    });
 
 }
 
 function SliderK__showPrev($slider) {
-  SliderK__showPost($slider, -1);
+    SliderK__showPost($slider, -1);
 }
 
 function SliderK__showNext($slider) {
-  SliderK__showPost($slider, 1);
+    SliderK__showPost($slider, 1);
 }
 
 function SliderK__showPost($slider, change) {
-  var $currentSlide = $slider.find('.slides > div.active');
-  var $postSlide = null;
-  var $firstSlide = $slider.find('.slides > div:first-child');
-  var $lastSlide = $slider.find('.slides > div:last-child');
+    var $currentSlide = $slider.find('.slides > div.active');
+    var $postSlide = null;
+    var $firstSlide = $slider.find('.slides > div:first-child');
+    var $lastSlide = $slider.find('.slides > div:last-child');
 
-  if ( change == 1 ) {
-    $postSlide = $currentSlide.next();
+    if (change == 1) {
+        $postSlide = $currentSlide.next();
 
-    if ( $postSlide.length == 0 ) {
-      $postSlide = $firstSlide;
+        if ($postSlide.length == 0) {
+            $postSlide = $firstSlide;
+        }
+    } else if (change == -1) {
+        $postSlide = $currentSlide.prev();
+
+        if ($postSlide.length == 0) {
+            $postSlide = $lastSlide;
+        }
     }
-  }
-  else if ( change == -1 ) {
-    $postSlide = $currentSlide.prev();
 
-    if ( $postSlide.length == 0 ) {
-      $postSlide = $lastSlide;
-    }
-  }
-
-  SliderK__show($slider, $postSlide.index());
+    SliderK__show($slider, $postSlide.index());
 }
 
 /* 초기화 */
 function SliderK__init() {
-  $('.slider-k').each(function(index, node) {
-    var $slider = $(node);
+    $('.slider-k').each(function (index, node) {
+        var $slider = $(node);
 
-    SliderK__initPageNav($slider);
-    SliderK__initSideBtns($slider);
-    SliderK__initAutoplay($slider);
-    SliderK__show($slider, 0);
-  });
+        SliderK__initPageNav($slider);
+        SliderK__initSideBtns($slider);
+        SliderK__initAutoplay($slider);
+        SliderK__show($slider, 0);
+    });
 }
 
 // 페이지 내비를 자동으로 만들어줍니다.
 function SliderK__initPageNav($slider) {
-  var currentIndex = $slider.find('.slides > div.active').index();
-  var slidesCount = $slider.find('.slides > div').length;
+    var currentIndex = $slider.find('.slides > div.active').index();
+    var slidesCount = $slider.find('.slides > div').length;
 
-  var html = '';
+    var html = '';
 
-  for ( var i = 0; i < slidesCount; i++ ) {
-    if ( i == currentIndex ) {
-      html += '<div class="active"><span>' + '</span></div>';
+    for (var i = 0; i < slidesCount; i++) {
+        if (i == currentIndex) {
+            html += '<div class="active"><span>' + '</span></div>';
+        } else {
+            html += '<div><span>' + '</span></div>';
+        }
     }
-    else {
-      html += '<div><span>' + '</span></div>';
-    }
-  }
 
-  html = '<div class="page-nav"><div><div>' + html + '</div><section></section></div></div>';
-  $slider.append(html);
+    html = '<div class="page-nav"><div><div>' + html + '</div><section></section></div></div>';
+    $slider.append(html);
 
-  $slider.find('.page-nav > div > div > div').click(function() {
-    SliderK__show($slider, $(this).index());
-  });
+    $slider.find('.page-nav > div > div > div').click(function () {
+        SliderK__show($slider, $(this).index());
+    });
 }
 
 // 사이드 버튼에 이벤트를 겁니다.
 function SliderK__initSideBtns($slider) {
-  $slider.find('.side-btns > div').click(function() {
-    var index = $(this).index();
+    $slider.find('.side-btns > div').click(function () {
+        var index = $(this).index();
 
-    if ( index == 0 ) {
-      SliderK__showPrev($slider);
-    }
-    else {
-      SliderK__showNext($slider);
-    }
-  });
+        if (index == 0) {
+            SliderK__showPrev($slider);
+        } else {
+            SliderK__showNext($slider);
+        }
+    });
 }
 
 function SliderK__initAutoplay($slider) {
-  var autoplay = $slider.data('autoplay');
+    var autoplay = $slider.data('autoplay');
 
-  $slider.data('autoplay-now-work', 'Y');
-
-  $slider.mouseenter(function() {
-    $slider.data('autoplay-now-work', 'N');
-  });
-
-  $slider.mouseleave(function() {
     $slider.data('autoplay-now-work', 'Y');
-  });
 
-  if ( autoplay != 'Y' ) {
-    return false;
-  }
+    $slider.mouseenter(function () {
+        $slider.data('autoplay-now-work', 'N');
+    });
 
-  var autoplayInterval = $slider.data('autoplay-interval');
+    $slider.mouseleave(function () {
+        $slider.data('autoplay-now-work', 'Y');
+    });
 
-  if ( typeof autoplayInterval == 'undefined' ) {
-    autoplayInterval = 3000;
-  }
-  else {
-    // 문자열을 숫자화
-    autoplayInterval = autoplayInterval * 1;
-  }
-
-  var autoplayDirIsLeft = $slider.data('autoplay-dir') == 'left';
-
-  setInterval(function() {
-    if ( $slider.data('autoplay-now-work') == 'Y' ) {
-      if ( autoplayDirIsLeft ) {
-        SliderK__showPrev($slider);
-      }
-      else {
-        SliderK__showNext($slider);
-      }
+    if (autoplay != 'Y') {
+        return false;
     }
-  }, autoplayInterval);
+
+    var autoplayInterval = $slider.data('autoplay-interval');
+
+    if (typeof autoplayInterval == 'undefined') {
+        autoplayInterval = 3000;
+    } else {
+        // 문자열을 숫자화
+        autoplayInterval = autoplayInterval * 1;
+    }
+
+    var autoplayDirIsLeft = $slider.data('autoplay-dir') == 'left';
+
+    setInterval(function () {
+        if ($slider.data('autoplay-now-work') == 'Y') {
+            if (autoplayDirIsLeft) {
+                SliderK__showPrev($slider);
+            } else {
+                SliderK__showNext($slider);
+            }
+        }
+    }, autoplayInterval);
 }
 
 $(document).ready(function () {
     $(".hd-tp-right ul li:last-child div").click(function () {
-        $(".hd-tp-right ul li:last-child").toggleClass("active");
+        $(".hd-tp-right ul li:last-child").addClass("active");
+    });
+    $(".hd-tp-right ul li:last-child .cancel-icon").click(function () {
+        $(".hd-tp-right ul li:last-child").removeClass("active");
     });
 
     $('.slider').slick({
@@ -162,6 +160,46 @@ $(document).ready(function () {
             return '<div class="page-btn"></div>';
         }
     });
-    
+
+    $('#section5 .sec-5-slider').slick();
+
+    $('#section6 .slider-wrap').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1
+    });
+
+    $('#section9 .slider-wrap').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1
+    });
+
     SliderK__init();
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
