036023717763eba6c27d42c6642f9b4db79daf0a
sohi0000 <sohi0000@naver.com>
2020-11-23 17:48:07 +0900

    2020-11-23 작업

--- gongcha/js/script.js
+++ gongcha/js/script.js
@@ -81,5 +81,21 @@ $(function () {
 
     section3_slider__init();
 
+    
+    function TabBox__init() {
+  $('[data-tab-head-item-name]').click(function() {
+    var $this = $(this);
+    var tabName = $this.attr('data-tab-name');
+    var itemName = $this.attr('data-tab-head-item-name');
+    // [for]
+    // 모든 것을 숨기고
+    $('[data-tab-name="' + tabName + '"]').removeClass('active');
+    
+    $('[data-tab-name="' + tabName + '"][data-tab-head-item-name="' + itemName + '"]').addClass('active');
+    $('[data-tab-name="' + tabName + '"][data-tab-body-item-name="' + itemName + '"]').addClass('active');
+  });
+}
+
+    TabBox__init();
 
 });
