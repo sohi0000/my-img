1ad50dd953f231220d5e1fbf41f4c0304f377797
sohi0000 <sohi0000@naver.com>
2020-12-02 17:54:14 +0900

    2020-12-02 작업

--- bluecom/js/script.js
+++ bluecom/js/script.js
@@ -1,56 +1,42 @@
-$(document).ready(function(){
-    
-    
+$(document).ready(function () {
+
+
     new fullpage('#fullpage', {
-//        navigation: true,
-//        navigationPosition: 'right',
-        
-        
-        
+        //        navigation: true,
+        //        navigationPosition: 'right',
+
+
     });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+
+    function BottomLine__init() {
+        var $itemBottomLine = $('.hd-box > .menu > .bottom-line');
+
+        $('.hd-box > .menu > ul > li').mouseenter(function () {
+            var $this = $(this);
+            var left = $this.position().left;
+            var width = $this.width();
+            $itemBottomLine.css('left', left);
+            $itemBottomLine.css('width', width);
+            $itemBottomLine.css('transition', '0.3s');
+        });
+
+        $('.hd-box > .menu > ul > li').eq(0).mouseenter();
+
+        $('.hd-box > .menu > ul').mouseleave(function () {
+            $itemBottomLine.css('left', '');
+            $itemBottomLine.css('transition', '1s');
+        });
+    }
+
+    BottomLine__init();
+
+
+
+
+
+
+
+
+
 });
